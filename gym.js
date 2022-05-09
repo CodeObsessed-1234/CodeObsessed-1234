694de790ad60f339dfc4d7ed04c1a31acf570fc8
CodeObsessed-1234 <86585422+CodeObsessed-1234@users.noreply.github.com>
2022-05-09 20:12:27 +0530

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,21 @@
+function anual(){
+    document.getElementById('table1-h3').innerText = "Experts";
+    document.getElementById('table1-h1').innerText = "$195/anum";
+    
+    document.getElementById('table2-h3').innerText = "Pro";
+    document.getElementById('table2-h1').innerText = "$495/anum";
+    
+    document.getElementById('table3-h3').innerText = "Legends";
+    document.getElementById('table3-h1').innerText = "$1195/anum";
+}
+function month(){
+    document.getElementById('table1-h3').innerText = "Experts";
+    document.getElementById('table1-h1').innerText = "$395/mo";
+    
+    document.getElementById('table2-h3').innerText = "Pro";
+    document.getElementById('table2-h1').innerText = "$995/mo";
+    
+    document.getElementById('table3-h3').innerText = "Legends";
+    document.getElementById('table3-h1').innerText = "$2195/mo";
+
+}
